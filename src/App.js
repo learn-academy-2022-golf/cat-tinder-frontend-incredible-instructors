eab271ace417a7fa15b3db42bf9c383c338e450d
WalkerTexas-coder <austinsrwalker@gmail.com>
2022-12-16 10:56:18 -0800

    Reactstrap, reactrouterdom, routes set up

--- src/App.js
+++ src/App.js
@@ -8,21 +8,35 @@ import CatNew from './pages/CatNew'
 import CatEdit from './pages/CatEdit'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
+import cats from './mockCats'
+import { Routes, Route } from 'react-router-dom'
 
 
 const App = () => {
+
+  console.log(cats)
+
   return (
   <>
     <Header />
-    <Home />
-    <CatIndex />
-    <CatShow />
-    <CatNew />
-    <CatEdit />
-    <NotFound />
+    <Routes >
+        <Route path='/' element={<Home />}/>
+        <Route path='/catindex' element={<CatIndex />} />
+        <Route path='/catshow' element={<CatShow />} />
+        <Route path='/catnew' element={<CatNew />} />
+        <Route path='/catedit' element={<CatEdit />} />
+        <Route path="*"  element={<NotFound/>} />
+    </Routes>
     <Footer />
   </>
   );
 }
 
 export default App;
+
+// <Home />
+//       <CatIndex />
+//       <CatShow />
+//       <CatNew />
+//       <CatEdit />
+//       <NotFound />
